b2314501dde9f30f825fa65f28c529d735584f56
Michael Brewer <brewer7@pdx.edu>
2024-06-27 15:43:39 -0700

    checkfile.py - script to help verify a .vtk *UNSTRUCTURED GRID*

---
+++ checkfile.py
@@ -0,0 +1,62 @@
+import vtk
+import sys
+
+def read_vtk_file(file_path):
+    reader = vtk.vtkUnstructuredGridReader()
+    reader.SetFileName(file_path)
+    reader.Update()
+
+    # Check if there were errors during the read process
+    error_code = reader.GetErrorCode()
+    if error_code != vtk.vtkErrorCode.NoError:
+        raise ValueError(f"Error reading VTK file: {vtk.vtkErrorCode.GetStringFromErrorCode(error_code)}")
+
+    return reader.GetOutput()
+
+def check_file(file_path):
+    try:
+        output = read_vtk_file(file_path)
+        dataset_arrays = []
+        fields = [
+            (output.GetPointData(), vtk.vtkDataObject.FIELD_ASSOCIATION_POINTS),
+            (output.GetCellData(), vtk.vtkDataObject.FIELD_ASSOCIATION_CELLS),
+        ]
+
+        for field in fields:
+            field_arrays, association = field
+            num_arrays = field_arrays.GetNumberOfArrays()
+
+            for i in range(num_arrays):
+                array = field_arrays.GetArray(i)
+                if array is not None:
+                    array_range = array.GetRange()
+                    dataset_arrays.append(
+                        {
+                            "text": array.GetName(),
+                            "value": i,
+                            "range": list(array_range),
+                            "type": association,
+                        }
+                    )
+
+        if dataset_arrays:
+            default_array = dataset_arrays[0]
+            default_min, default_max = default_array.get("range")
+            
+        else:
+            print("No arrays found in the dataset.")
+            default_min, default_max = None, None
+            
+        print(f"Input file: '{file_path}' successfully checked...")
+        print(f"The default_min: {default_min} and default_max: {default_max}")
+
+    except ValueError as e:
+        print(f"An error occurred analyzing the file '{file_path}'... ** ERROR: {e} **")
+    
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script_name.py <path_to_vtk_file>")
+    else:
+        file_path = sys.argv[1]
+        check_file(file_path)
